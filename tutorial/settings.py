6bbefc3fcd8e8be8995f0c94b2cebf97935b49d4
Julião Martins <indianmobiledeveloper022@gmail.com>
2026-02-20 14:29:59 +0900

    feat: allow all hosts for development configuration

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8==)nk99ocp$ie
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
